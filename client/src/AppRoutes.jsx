0c58d3b6f346fbd6ab5c5a04f2931582e1a5ef66
Tymoteusz Wisniewski <tymoteusz.wisniewski95@gmail.com>
2024-05-06 16:01:23 +0100

    added routes, react carousel and small changes

---
+++ client/src/AppRoutes.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Homepage from './components/HomePage/Homepage'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import AppPaths from "./lib/appPaths";
+import ProductPage from "./components/ProductPage/ProductPage";
+
+
+ class AppRoutes extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar></Navbar>
+
+      <Routes>
+          <Route path={AppPaths.HOME} element={<Homepage/>} />
+          <Route path={AppPaths.TEMP} element={<ProductPage/>} />
+        </Routes>
+        <Footer></Footer>
+
+        </BrowserRouter>
+    );
+  }
+}
+
+export default AppRoutes
