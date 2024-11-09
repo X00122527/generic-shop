97aeb832cc45bf9602635cd44cd574bb063e273f
Tymoteusz Wisniewski <tymoteusz.wisniewski95@gmail.com>
2024-11-09 23:20:44 +0000

    Google authentication adjustments

--- client/src/components/Sign/GoogleSSO.jsx
+++ client/src/components/Sign/GoogleSSO.jsx
@@ -1,57 +1,65 @@
 import { useRef, useEffect } from 'react';
 import ApiEndpoints from "../../api/apiEndpoints";
 import ServerUrl from "../../api/serverUrl";
+import AppPaths from '../../lib/appPaths'
+import { useNavigate } from "react-router-dom";
+import CookieUtil from "../../util/cookieUtil";
+
+
 
 const GoogleSSO = () => {
+  const g_sso = useRef(null);
+  let navigate = useNavigate();
 
-    const g_sso = useRef(null);
 
-    useEffect(() => {
-        if (g_sso.current) {
-            google.accounts.id.initialize({
-                client_id: "939780826507-q2k2o2pqm48ie33bsncb1ag6judrord2.apps.googleusercontent.com",
-                callback: (res, error) => {
-                    console.log('pushing: ',JSON.stringify(res));
-                    const options = {
-                        method: 'POST',
-                        headers: {
-                          Accept: "application/json, text/plain",
-                          "Content-Type": "application/json; charset=UTF-8",
-                        },
-                        body: res //JSON.stringify(res)
-                      };
-                      const url = ServerUrl.BASE_URL + ApiEndpoints.GOOGLE_AUTH;
-                      let successLoginData;
-                      fetch(url, options)
-                        .then(response => {
-                          if (!response.ok) {
-                            throw new Error('Network response was not ok');
-                          }
-                          return response.json();
-                        })
-                        .then(data => {
-                          console.log(data);
-                        })
-                        .catch(error => {
-                          console.error('Fetch error:', error);
-                        });
-                    // This is the function that will be executed once the authentication with google is finished
-                },
+  useEffect(() => {
+    if (g_sso.current) {
+      google.accounts.id.initialize({
+        client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+        callback: (res, error) => {
+          const formData = new FormData();
+          formData.append('credential', res['credential']);
+          formData.append('site', "google");
+          const options = {
+            method: 'POST',
+            body: formData
+          };
+          const url = ServerUrl.BASE_URL + ApiEndpoints.GOOGLE_AUTH;
+          fetch(url, options)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.json();
+            })
+            .then((data) => {
+              console.log(data);
+              Object.keys(data).forEach((key) => {
+                CookieUtil.setCookie(key, data[key]);
+            })
+            })
+            .catch(error => {
+              console.error('Fetch error:', error);
             });
-            google.accounts.id.renderButton(g_sso.current, {
-                theme: 'outline',
-                size: 'large',
-                type: 'standard',
-                text: 'signin_with',
-                shape: 'rectangular',
-                logo_alignment: 'left',
-                width: '220',
-            });
-        }
-    }, [g_sso.current]);
+          navigate(AppPaths.SHOP, {replace: true});
+          window.location.reload();
+
+        },
+      });
+      google.accounts.id.renderButton(g_sso.current, {
+        theme: 'outline',
+        size: 'large',
+        type: 'standard',
+        text: 'signin_with',
+        shape: 'rectangular',
+        logo_alignment: 'left',
+        width: '100%',
+      });
+    }
+  }, [g_sso.current]);
 
 
-    return (<div ref={g_sso} />);
+  return (<div ref={g_sso} />);
 }
 
 
