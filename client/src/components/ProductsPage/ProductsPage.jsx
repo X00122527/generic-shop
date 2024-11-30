46ce8746b1dd4a0aa92c09a9828ac4ddb7e556da
Tymoteusz Wisniewski <tymoteusz.wisniewski95@gmail.com>
2024-11-30 23:53:23 +0000

    added pagination to products page

--- client/src/components/ProductsPage/ProductsPage.jsx
+++ client/src/components/ProductsPage/ProductsPage.jsx
@@ -7,7 +7,7 @@ import withReactContent from 'sweetalert2-react-content'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import FilterSortBody from './FilterSortBody'
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useSearchParams, useLocation } from 'react-router-dom';
 import AppPaths from '../../lib/appPaths';
 
 function ProductsPage(props) {
@@ -16,6 +16,9 @@ function ProductsPage(props) {
     const [isLoading, setIsLoading] = useState(true);
     const [searchParams] = useSearchParams();
     const filter = searchParams.toString();
+    const [orderData, setOrderData] = useState("");
+    const [paginationArray, setPaginationArray] = useState([]);
+    const location = useLocation();
 
     useEffect(() => {
         fetchProducts();
@@ -23,7 +26,7 @@ function ProductsPage(props) {
 
     const fetchProducts = () => {
         console.log("fetching products...",filter);
-        
+        var url = ServerUrl.BASE_URL + ApiEndpoints.PRODUCTS + location.search // + "?"+filter;
         const options = {
             method: 'GET',
             headers: {
@@ -32,7 +35,13 @@ function ProductsPage(props) {
             },
         };
 
-        const url = ServerUrl.BASE_URL + ApiEndpoints.PRODUCTS + "?"+filter;
+
+
+        // if(query !== ""){
+        //     url += query + orderData;
+        // }else{
+        //     url += orderData;
+        // }
 
         fetch(url, options)
             .then(response => {
@@ -42,12 +51,38 @@ function ProductsPage(props) {
                 return response.json();
             })
             .then(data => {
-                setProductList(data.results);
+                console.log("data.results: ",data.results)
+                setProductList(data);
+                calculatePagination({ total_pages: data.total_pages, current_page: data.current_page });
                 setIsLoading(false);
             })
             .catch(error => {
                 console.error('Fetch error:', error);
             });
+    }
+
+    const calculatePagination = (pagination) => {
+        var array = [];
+        var add = 0;
+        if (pagination.current_page === 1) {
+            add = 2;
+        } else if (pagination.current_page === 2) {
+            add = 1;
+        }
+
+        if (pagination.current_page === pagination.total_pages - 1) {
+            add = -1;
+        } else if (pagination.current_page === pagination.total_pages) {
+            add = -2;
+        }
+
+        for (var i = -2 + add; i <= 2 + add; i++) {
+            if (pagination.current_page + (i) > 0 & pagination.current_page + (i) <= pagination.total_pages) {
+                array.push(pagination.current_page + (i))
+            }
+        }
+        setPaginationArray(array);
+
     }
 
     if(isLoading){
@@ -61,16 +96,16 @@ function ProductsPage(props) {
                 <FilterSortBody {...props}></FilterSortBody>
                 <div id="related-items" className='col-span-2 mt-10 '>
                     {/* product card goes here, can introduce also product slider */}
-                    {productList.length ===0 &&
+                    {productList.results.length ===0 &&
                         <p>There is no products that would match your query of {filter}</p>
                         }
 
-                    <div id='products' className='grid w-full grid-cols-2 gap-4 mx-auto mt-2 md:grid-cols-4 '>
+                    <div id='products' className='grid w-full grid-cols-2 gap-8 mx-auto mt-2 md:grid-cols-4 '>
                         
-                        {productList.map((product, index) => 
+                        {productList.results.map((product, index) => 
                             <Link to={"/" + AppPaths.PRODUCT.replace(':productId', product.id)} key={index}>
                             <div id="product" className='text-center' >
-                                <img src={product.images[0].image} className='w-full'></img>
+                                <img src={"http://192.168.178.82:8000" + product.images[0].thumbnail_url} className='w-full'></img>
                                 {/* <span>{product.images[0]}</span> */}
                                 <p className='text-md font-shadows'>{product.title}</p>
                                 <p className='price'>{product.currency} {product.price}</p>
@@ -79,6 +114,24 @@ function ProductsPage(props) {
                         )}
                     </div>
                 </div>
+
+                <div className='bottom-0 flex justify-center mt-4'>
+                
+                {paginationArray.map((pag, index) => (
+                    <Link
+                        reloadDocument
+                        key={index}
+                        to={location.search === "" ?
+                            "?page=" + pag :
+                            //replacing either ?page or &page if exists to prevent adding to url endless pages qs
+                            location.search.replace(/([?&])page=\d+(&|$)/, (match, p1, p2) => p1 === '?' ? '?' : p2 ? '&' : '') + "&page=" + pag}
+                        className={`${pag === productList.current_page ? "border-2" : ""} flex w-8 h-8 text-2xl border-black rounded-full justify-center items-center`}
+                    >
+                        {pag}
+                    </Link>
+                ))}
+            </div>
+
             </div>
         </>
     )
