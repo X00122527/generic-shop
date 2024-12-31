265d8a46d04e04c5ae24de52998809c17155110c
Tymoteusz Wisniewski <tymoteusz.wisniewski95@gmail.com>
2024-12-31 14:45:06 +0000

    frontend fixes for logging and sign ups

--- client/src/components/Sign/Login.jsx
+++ client/src/components/Sign/Login.jsx
@@ -40,7 +40,7 @@ function Login({ location }) {
     };
     const url = ServerUrl.BASE_URL + ApiEndpoints.LOGIN;
 
-    var successLoginData = await  fetch(url, options)
+    var successLoginData = await fetch(url, options)
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -55,7 +55,7 @@ function Login({ location }) {
       Object.keys(successLoginData).forEach((key) => {
         CookieUtil.setCookie(key, successLoginData[key]);
       });
-      navigate(AppPaths.SHOP, {replace: true});
+      navigate(AppPaths.SHOP, { replace: true });
       window.location.reload();
     }
   };
@@ -83,56 +83,55 @@ function Login({ location }) {
     // onClick={googleAuth}
     // className='h-12 px-2 bg-white border-2 border-black rounded-lg w-fit'>Sign up with google</button>
     // </div>
-    <div className='container grid w-1/3 p-10 mx-auto my-auto border-2 border-gray-400 rounded-lg gap-y-4'>
+
+
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
       {getLoginMessage()}
-      <div>
-        <span>Log in to your account</span>
-      </div>
-      <div className='grid gap-2'>
-        <form onSubmit={handleSubmit(onSubmit)} className="">
-          <div className="authFieldContainer">
-            <input
-              className="w-full my-2"
-              type="email"
-              placeholder="Email"
-              {...register("username", { required: true })}
-            />
-            {errors.username && (
-              <p className="requiredFieldError">This field is required</p>
-            )}
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Log in to your account
+            </h1>
+            <GoogleSSO></GoogleSSO>
+            <div class="inline-flex items-center justify-center w-full">
+              <hr class="w-full h-px bg-gray-200 border-0 dark:bg-gray-700" />
+              <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">or</span>
+            </div>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 md:space-y-6">
+              <div>
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" name="email" id="email"
+                  {...register("username", { required: true })}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+              </div>
+              <div>
+                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••"
+                  {...register("password", { required: true })}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                  </div>
+                </div>
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+              </div>
+              <button type="submit" className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet? <Link to="/signup"><span className="font-medium text-black hover:underline dark:text-primary-500">Click here</span></Link>
+              </p>
+            </form>
           </div>
-          <div className="authFieldContainer">
-            <input
-              className="w-full"
-              type="password"
-              placeholder="Password"
-              {...register("password", { required: true })}
-            />
-            {errors.password && (
-              <p className="requiredFieldError">This field is required</p>
-            )}
-          </div>
-          <br />
-          <button className="w-full p-2 border-2 border-black btn-block" type="submit">
-            Login
-          </button>
-          <p>OR</p>
-          <GoogleSSO></GoogleSSO>
-
-        </form>
-
-        <p id="authFormFooter">
-          Don't have any account! <Link to="/signup">Click here</Link> to
-          singup.
-        </p>
-
+        </div>
       </div>
-      
 
 
-
-
-    </div>
   )
 }
 
--- client/src/components/Sign/Signup.jsx
+++ client/src/components/Sign/Signup.jsx
@@ -63,17 +63,17 @@ const Signup = () => {
   };
 
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
-      <div class="flex flex-col items-center px-6 mx-auto md:h-screen lg:py-0">
+      <div class="flex flex-col items-center px-6 mx-auto  lg:py-0">
 
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Create an account
             </h1>
-            <div>
             <GoogleSSO></GoogleSSO>
-            <p className="text-center">or</p>
+            <div class="inline-flex items-center justify-center w-full">
+              <hr class="w-full h-px bg-gray-200 border-0 dark:bg-gray-700" />
+              <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">or</span>
             </div>
             <form onSubmit={handleSubmit(onSubmit)} class="space-y-4 md:space-y-6" >
               <div>
@@ -125,13 +125,12 @@ const Signup = () => {
               </div>
               <button type="submit" class="w-full text-white bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
               <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account? <Link to="/login">Log in here</Link>
+                Already have an account? <Link to="/login"><span className="font-medium text-black hover:underline dark:text-primary-500">Log in here</span></Link>
               </p>
             </form>
           </div>
         </div>
       </div>
-    </section>
   );
 };
 
